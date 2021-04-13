92a525b2a86f6bb2e3c6ea3da40cab65ed718dc3
Jens-k-m-m <jensmonteiro@gmail.com>
2021-04-13 17:40:56 +0200

    Jens sout

--- funJens13.4.2021/src/com/company/Main.java
+++ funJens13.4.2021/src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hej jens :)");
+        System.out.print("% Jens er klar");
 	// write your code here
     }
 }
